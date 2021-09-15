5e77149bd98e786e528ac914304838e17fff293f
Niall Barber <niall.barber@actual-experience.com>
2021-09-15 14:02:30 +0100

    Added some more logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
 	import {generatePassword} from './utils/generatePassword';
 	import {copyToClipboard} from './utils/copyToClipboard';
+	import {handleBackgroundChange} from './utils/background';
 
 	let range: number = 12;
-	let pw: string = "";
 	let isLetter: boolean = true;
 	let isNumber: boolean = true;
 	let isSymbol: boolean = false;
 
-	$: pwLength = pw.length;
+	function handleChange(e: Event) {
+		const value = (<HTMLInputElement>e.target).value
+		range = parseInt(value);
+	}
+
+	$: backgroundColor = handleBackgroundChange(range);
 	$: pwR = generatePassword(range, isLetter, isNumber, isSymbol);
 </script>
 
-<main>
+<main class="{backgroundColor}">
 	<div>
 		<p>{pwR}</p>
 		<p>password strength</p>
@@ -20,29 +25,86 @@
 			type="range"
 			id="range"
 			name="password"
+			class="slider"
 			min="0"
 			max="40"
 			step="1"
 			value={range}
-			on:change={(e) => (range = e.target.value)}
-			style="background: linear-gradient(to right, rgb(255, 255, 255) '{(range) * 2.5}'%, rgba(255, 255, 255, 0.4) 0%)"
+			on:change={handleChange}
+			style="background: linear-gradient(to right, rgb(255, 255, 255) {range * 2.5}%, rgba(255, 255, 255, 0.4) 0%)"
 		/>
-		<p>Length ({pwLength})</p>
-		<p>Range is {range}</p>
+		<p>Length ({range})</p>
+		<p>{backgroundColor}</p>
 	</div>
 
 	<div>
-		<button on:click={() => (isLetter = !isLetter)}>Letters</button>
-		<button on:click={() => (isNumber = !isNumber)}>Numbers</button>
-		<button on:click={() => (isSymbol = !isSymbol)}>Symbols</button>
+		<input
+			type="checkbox"
+			class="{isLetter && 'letter'}"
+			id="letters"
+			on:click={() => (isLetter = !isLetter)}
+		/>
+		<label for="letters">Letters</label>
+		<button
+			class="{isNumber && 'number'}"
+			on:click={() => (isNumber = !isNumber)}
+		>
+			Numbers
+		</button>
+		<button
+			class="{isSymbol && 'symbol'}"
+			on:click={() => (isSymbol = !isSymbol)}
+		>
+			Symbols
+		</button>
 	</div>
 
 	<button on:click={() => copyToClipboard(pwR)}>Copy password</button>
 </main>
 
-<style>
+<style lang="scss">
+	main {
+		background: var(--black);
+
+		&.mega {
+			background: var(--mega);
+		}
+		&.good {
+			background: var(--good);
+		}
+		&.medium {
+			background: var(--medium);
+		}
+		&.bad {
+			background: var(--bad);
+		}
+	}
+
 	p {
 		margin: 0;
-		color: purple;
+		color: var(--white);
+	}
+
+	button {
+		color: var(--white);
+
+		&.letter {
+			background: red;
+		}
+		&.number {
+			background: blue;
+		}
+		&.symbol {
+			background: green;
+		}
+	}
+
+	.slider {
+		align-self: center;
+    appearance: none;
+    width: 100%;
+    border-radius: 8px;
+    height: 8px;
+    cursor: pointer;
 	}
 </style>
